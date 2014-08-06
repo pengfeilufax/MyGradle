f8c231114daee69567880bd73feb0a880353b1e0
pengfeilufax <pengfei_jesse@163.com>
2014-08-06 22:57:43 +0800

    second

--- src/main/java/org/gradle/sample/Greeter.java
+++ src/main/java/org/gradle/sample/Greeter.java
@@ -8,6 +8,7 @@ public class Greeter {
     public String getGreeting() throws Exception {
         LogManager.getRootLogger().info("generating greeting.");
         InputStream greetingStr = getClass().getResourceAsStream("/greeting.txt");
+       
         try {
             return IOUtils.toString(greetingStr).trim();
         }
